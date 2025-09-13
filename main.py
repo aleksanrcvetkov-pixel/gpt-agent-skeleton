155e10e63a0fcc9f653d55d2a431509064ef82c8
aleksanrcvetkov-pixel <aleksanrcvetkov@gmail.com>
2025-09-13 21:25:42 +0300

    Update main.py Добавлен эндпоинт /about

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, world!"}
+@app.get("/about")
+def about():
+    return {"info": "Это мой первый FastAPI проект на GitHub!"}
